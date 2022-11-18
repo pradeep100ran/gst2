64baeb6432ae0d601006a7ca4ec0415726e87e8a
pradeep100ran <103431083+pradeep100ran@users.noreply.github.com>
2022-11-18 12:07:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$aaa=file_get_contents('http://139.59.221.16:4001/api/scrips/feed?channel_names=nse_fno_nifty_121022');
+$aaa=file_get_contents('http://139.59.221.16:4001/api/scrips/feed?channel_names=nse_fno_nifty_112022');
 echo($aaa);
 ?>
